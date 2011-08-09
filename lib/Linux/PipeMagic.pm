640b7318f9556c1051a6568fc0bd5748d2da5c87
Dave Lambley <dave@lambley.me.uk>
2011-08-09 08:48:04 +0100

    New version.

--- lib/Linux/PipeMagic.pm
+++ lib/Linux/PipeMagic.pm
@@ -32,7 +32,7 @@ our @EXPORT = qw(
     SPLICE_F_GIFT
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
