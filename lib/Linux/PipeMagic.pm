8de5a3d2c373a2a4e854512f4c39f7a7a7d8124f
Dave Lambley <dave@lambley.me.uk>
2011-08-09 08:53:52 +0100

    Avoid exporting everything by default.

--- lib/Linux/PipeMagic.pm
+++ lib/Linux/PipeMagic.pm
@@ -18,19 +18,18 @@ our @ISA = qw(Exporter);
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-    systee
+	systee
     syssplice
     SPLICE_F_MOVE
     SPLICE_F_NONBLOCK
     SPLICE_F_MORE
     SPLICE_F_GIFT
-);
+
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw();
 
 our $VERSION = '0.02';
 
