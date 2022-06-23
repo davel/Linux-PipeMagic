46e02d60ebeffee71f8eb572ccb3822d284ce8d4
Dave Lambley <dave@lambley.me.uk>
2022-06-23 11:12:56 +0100

    Bump version

--- lib/Linux/PipeMagic.pm
+++ lib/Linux/PipeMagic.pm
@@ -31,7 +31,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = '0.04_01';
+our $VERSION = '0.04';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
